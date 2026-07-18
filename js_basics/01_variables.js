20af7a494017c6c131b0a5d17ecc754bc9e3a1ea
Rahul Rajput <rahulrajput.codes@gmail.com>
2026-07-18 14:10:39 +0530

    Start tutorial

---
+++ js_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 133324
+let accountEmail = "dhbdhbs"
+var accountPass = "dbchsbjsb"
+accountCity = "GOA"
+
+/* we should avoid using var 
+ because it has the issues of scope 
+ */
+
+// accountId =2                     // not allowed 
+
+accountEmail = "ndkdjdjs"
+accountPass = "jd djs ds ds"
+accountCity = "jsnjsnns"
+console.log(accountId)
+console.table([accountEmail,accountId,accountPass,accountCity])
---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treats all JS code as a newer version 
+
+// alert(3+3) // we are using nodejs , not browser
+// code must be readable enough too 
+// console.log(6+6); console.log(8)  // bad practice 
