86d5043f4a5096bc7d74704c274c3b882e7dec8d
Rahul Rajput <rahulrajput.codes@gmail.com>
2026-07-18 14:18:20 +0530

    basic datatype

--- js_basics/02_datatypes.js
+++ js_basics/02_datatypes.js
@@ -3,3 +3,24 @@
 // alert(3+3) // we are using nodejs , not browser
 // code must be readable enough too 
 // console.log(6+6); console.log(8)  // bad practice 
+
+// number - has a range 
+let age = 18
+
+// string - " "
+let name = "rahul"
+
+// boolean - true / false
+let eligible = true 
+
+// null - standalone value 
+//undefined
+
+//symbol - uniqueness
+
+//object
+
+console.log(typeof(age))
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
